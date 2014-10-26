c70d457fe2bbc0181e7f7e09e92a19252786bdb6
Serhey Shmyg <serhey.shmyg.all@gmail.com>
2014-10-26 09:06:08 +0200

    change local server, remove sass support yet

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,21 +1,22 @@
 var gulp		= require('gulp'),
 	less		= require('gulp-less-sourcemap'),
-	sass		= require('gulp-ruby-sass'),
-	clean		= require('gulp-rimraf'),
+	clear		= require('gulp-rimraf'),
 	jade		= require('gulp-jade'),
-	sprite		= require('gulp.spritesmith'),
 	browserify	= require('browserify'),
 	watchify	= require('watchify'),
 	source		= require('vinyl-source-stream'),
 	rename		= require('gulp-rename'),
-	webserver	= require('gulp-webserver'),
-	changed		= require('gulp-changed'),
+	browserSync = require('browser-sync'),
+	jadeInherit = require('gulp-jade-inheritance'),
 	extend		= require('gulp-extend'),
+	ignore		= require('gulp-ignore'),
 	loc = {
 		root		: 'app',
 		build		: 'build',
 
 		markup		: 'app/markup',
+		markupCss	: 'app/markup/css',
+		markupJs	: 'app/markup/js',
 
 		jade		: 'app/jade',
 		jadeData	: './app/jade/files/data',
@@ -24,8 +25,6 @@ var gulp		= require('gulp'),
 		js			: 'app/js',
 
 		css			: 'app/css',
-		less		: 'app/css/less',
-		sass		: 'app/css/sass',
 
 		images		: 'app/images',
 
@@ -33,24 +32,10 @@ var gulp		= require('gulp'),
 		jsInit		: './app/js/init.js',
 		jsMin		: 'main.min.js',
 
-
 		/* CSS */
 		allCssMinName	: 'all.min',
-		sourceMapURL	: '../css/all.min.css.map',
-		lessAll			: 'app/css/less/all.less',
-		sassAll			: 'app/css/sass/all.scss'
-	},
-	CSS_ENGINE	= 'less', //less, sass
-	SERVER_CONF	= {
-		livereload	: true,
-		port		: 8080,
-		open		: '/markup',
-		fallback	: '/markup/index.html',
-		directoryListing: {
-			enable: true,
-			path: loc.root,
-			options: undefined
-		}
+		sourceMapURL	: 'all.min.css.map',
+		lessAll			: 'app/css/all.less'
 	},
 	BROWSERIFY_CONF = {
 		debug : true,
@@ -58,36 +43,74 @@ var gulp		= require('gulp'),
 		bundleConfigs : [
 			{
 				entries		: loc.jsInit,
-				dest		: loc.js,
+				dest		: loc.markupJs,
 				outputName	: loc.jsMin
 			}
 		]
-	};
+	},
+	SERVER_CONF = {
+		notify		: false,
+		directory	: true,
+		server : {
+			baseDir : [loc.markup],
+		},
+		files : [
+			loc.root + '/**',
+			'!' + loc.root + '/**.map',
+			'!' + loc.jade + '/**.jade'
+		]
+	},
+	JADE_FILES = loc.jade + '/*.jade';
 
-gulp.task('js', function() {
-	//https://github.com/greypants/gulp-starter
-	var applyBrowserify = function(conf) {
-		var bundler = browserify({
-				cache		: {},
-				packageCache: {},
-				fullPaths	: true,
-				entries		: conf.entries,
-				extensions	: BROWSERIFY_CONF.extensions,
-				debug		: BROWSERIFY_CONF.debug
-			}),
-			bundle = function() {
-				return bundler
-						.bundle()
-						.pipe(source(conf.outputName))
-						.pipe(gulp.dest(conf.dest))
-			};
 
-		bundler = watchify(bundler);
+gulp.task('setWatch', function() {
+	global.isWatching = true;
+});
 
-		bundler.on('update', bundle);
+//https://github.com/greypants/gulp-starter
+gulp.task('js', function(callback) {
+	var bundleQueue = BROWSERIFY_CONF.bundleConfigs.length,
+		applyBrowserify = function(conf) {
+			var bundler = browserify({
+					cache		: {},
+					packageCache: {},
+					fullPaths	: true,
+					entries		: conf.entries,
+					extensions	: BROWSERIFY_CONF.extensions,
+					debug		: BROWSERIFY_CONF.debug
+				}),
+				handleErrors = function() {
+					console.log('BUNDLE ERROR!!!!!!');
+				},
+				reportFinished = function() {
+					//console.log(conf.outputName)
 
-		return bundle();
-	};
+					if(bundleQueue) {
+						bundleQueue--;
+
+						// If queue is empty, tell gulp the task is complete.
+						// https://github.com/gulpjs/gulp/blob/master/docs/API.md#accept-a-callback
+						if(bundleQueue === 0) callback();
+					}
+				},
+				bundle = function() {
+					//console.log(conf.outputName);
+
+					return bundler
+							.bundle()
+							.on('error', handleErrors)
+							.pipe(source(conf.outputName))
+							.pipe(gulp.dest(conf.dest))
+							.on('end', reportFinished);
+				};
+
+			if (global.isWatching) {
+				bundler = watchify(bundler);
+				bundler.on('update', bundle);
+			}
+
+			return bundle();
+		};
 
 	BROWSERIFY_CONF.bundleConfigs.forEach(applyBrowserify);
 });
@@ -103,31 +126,21 @@ gulp.task('less', function () {
 				sourceMap			: true,
 				compress			: true,
 				sourceMapURL		: loc.sourceMapURL,
-				sourceMapBasepath	: loc.less
+				sourceMapBasepath	: loc.css
 			}))
-			.pipe(gulp.dest(loc.css));
-});
-
-gulp.task('sass', function () {
-	return gulp.src(loc.sassAll)
-			.pipe(rename({
-				basename	: loc.allCssMinName,
-				extname		: '.css'
-			}))
-			.pipe(sass({
-				sourcemap	: loc.css + '/' + loc.allCssMinName + '.css.map',
-				style		: 'compressed'
-			}))
-			.pipe(gulp.dest(loc.css));
+			.pipe(gulp.dest(loc.markupCss));
 });
 
 gulp.task('jade', function() {
-	return gulp.src(loc.jade + '/*.jade')
-			.pipe(changed(loc.markup))
+	var exclude = __dirname + '/'+ loc.jade + '/files/**/*.html';
+
+	return gulp.src(JADE_FILES)
+			.pipe(jadeInherit({basedir: loc.jade}))
 			.pipe(jade({
 				locals	: require(loc.jadeData + '/' + loc.jadeDataRes),
 				pretty	: true
 			}))
+			.pipe(ignore.exclude(exclude))
 			.pipe(gulp.dest(loc.markup));
 });
 
@@ -137,25 +150,19 @@ gulp.task('mergeJson', function() {
 			.pipe(gulp.dest(loc.jadeData));
 });
 
-gulp.task('webserver', function() {
-	return gulp
-			.src(loc.root)
-			.pipe(webserver(SERVER_CONF));
+gulp.task('webserver', ['build'], function() {
+	browserSync(SERVER_CONF);
 });
 
-gulp.task('clean', function(cb) {
+gulp.task('clear', function(cb) {
 	return gulp
 			.src([
-				loc.jsMin,
 				loc.build,
 				loc.markup,
-				loc.css + '/' + loc.allCssMinName + '.css',
-				loc.css + '/' + loc.allCssMinName + '.css.map',
-				loc.js + '/lib',
 				'./node_modules',
 				'npm-debug.log',
 			], {read : false})
-			.pipe(clean({force : true}));
+			.pipe(clear({force : true}));
 });
 
 gulp.task('copy', function() {
@@ -166,26 +173,18 @@ gulp.task('copy', function() {
 	gulp
 		.src(loc.images + '/*')
 		.pipe(gulp.dest(loc.build + '/images'));
-
-	gulp
-		.src([
-			loc.minJs
-		])
-		.pipe(gulp.dest(loc.build + '/js'));
-
-	gulp
-		.src([
-			loc.css + '/' + loc.allCssMinName + '.css',
-			loc.css + '/' + loc.allCssMinName + '.css.map'
-		])
-		.pipe(gulp.dest(loc.build + '/css'));
 });
 
-gulp.task('watch', function() {
-	gulp.watch(loc.less + '/**/*.less', ['mergeJson', 'less']);
-	gulp.watch(loc.sass + '/**/*.scss', ['mergeJson', 'sass']);
-	gulp.watch(loc.jade + '/**/*.jade', ['mergeJson', 'jade']);
+gulp.task('watch', ['setWatch', 'webserver'], function() {
+	var jadeWatcher;
+
+	jadeWatcher = gulp.watch(loc.jade + '/**/*.jade', ['jade', browserSync.reload]);
+	gulp.watch(loc.css + '/**/*.less', ['less']);
+
+	jadeWatcher.on('change', function(e) {
+		JADE_FILES = e.path;
+	});
 });
 
-gulp.task('default', ['mergeJson', 'jade', CSS_ENGINE, 'js', 'webserver', 'watch']);
-gulp.task('build', ['mergeJson', 'jade', CSS_ENGINE, 'js', 'copy']);
+gulp.task('default', ['watch']);
+gulp.task('build', ['js', 'jade', 'less']);
--- app/js/modules/module-test/index.js
+++ app/js/modules/module-test/index.js
@@ -1,3 +1,3 @@
 module.exports = function() {
-	console.log( require('./templates/index.html') );
+	console.log( require('./templates/index.html'));
 }
