f1d3afd2ee1e905f66307364d664c8869434d4a9
Serhey Shmyg <serhey.shmyg@gmail.com>
2015-10-28 15:40:07 +0200

    Replace compass to libsass(gulp-sass)

--- Gulpfile.js/tasks/css.js
+++ Gulpfile.js/tasks/css.js
@@ -1,18 +1,27 @@
 var gulp             = require('gulp'),
-    gulpCompass      = require('gulp-compass'),
+    gulpIf           = require('gulp-if'),
+    gulpSass         = require('gulp-sass'),
+    sourcemaps       = require('gulp-sourcemaps'),
+    autoprefixer     = require('gulp-autoprefixer'),
+    neat             = require('node-neat').includePaths,
     path             = require('path'),
     handleTaskConfig = require('../helpers/taskConfigHandler'),
     handleErrors     = require('../helpers/errorsHandler'),
-    configGlob       = require(path.join(process.cwd(), 'config.json'));
+    configGlob       = require(path.join(process.cwd(), 'config.json')),
+    isDev            = configGlob.isDev;
 
 module.exports = function(config) {
     config = handleTaskConfig('css', config);
-    config.sass && (config.sass.sourcemap = configGlob.isDev);
+
+    config.sass && (config.sass.includePaths = ['styles'].concat(neat));
 
     gulp.task(config.taskName, function() {
         return gulp.src(config.src)
-                .pipe(gulpCompass(config.sass))
+                .pipe(gulpIf(isDev, sourcemaps.init()))
+                .pipe(gulpSass(config.sass))
                 .on('error', handleErrors)
+                .pipe(autoprefixer(config.autoprefixer))
+                .pipe(gulpIf(isDev, sourcemaps.write()))
                 .pipe(gulp.dest(config.dest));
     });
 
--- README.md
+++ README.md
@@ -2,8 +2,6 @@
 
 ##Dependencies
 1. [nodejs](http://nodejs.org/)
-2. Ruby
-3. SASS, COMPASS - (gem update --system && sudo gem install sass compass)
 
 ##Start:
 ```shell
@@ -15,6 +13,10 @@
 
 ##Features:
 1. Multiple bundles - every file from `entries` folder will be build into separate bundle.
-2. Free to use __es6__ syntax into javascript files. And mix it with __es5__ or __es3__;
-3. `require('template.html')` - no probs;
+2. Free to use __es6__ syntax into javascript files. And mix it with __es5__ or __es3__.
+3. `require('template.html')` - no probs.
 4. Use any tasks with different config `require('./tasks/jade')(diffConfig)`. How to use `diffConfig` see `config.json` in root folder.
+5. __SASS__ (`libsass`) for style.
+    - autoprefixer (vendor prefixer 3 latest browser version)
+    - bourbon (mixins)
+6. __JADE__ for templating
