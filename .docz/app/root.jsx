e332cfa21fc127b190ca6466889299bf1b3e8b16
Serhey Shmyg <serhey.shmyg@gmail.com>
2018-09-04 11:54:56 +0300

    Update babel to 7

--- .docz/app/root.jsx
+++ .docz/app/root.jsx
@@ -2,33 +2,16 @@ import React from 'react'
 import { hot } from 'react-hot-loader'
 import Theme from 'docz-theme-default'
 
-const socket = new WebSocket(`ws://127.0.0.1:8089`)
+import { imports } from './imports'
+import db from './db.json'
 
-class Root extends React.Component {
-  state = {
-    config: {},
-    entries: {},
-  }
-
-  async componentDidMount() {
-    socket.onmessage = ev => {
-      const message = JSON.parse(ev.data)
-
-      if (message.type === 'docz.entries') {
-        this.setState({ entries: message.data })
-      }
-
-      if (message.type === 'docz.config') {
-        this.setState({ config: message.data })
-      }
-    }
-  }
-
-  render() {
-    const { imports } = this.props
-
-    return <Theme {...this.state} imports={imports} hashRouter={false} />
-  }
-}
+const Root = () => (
+  <Theme
+    db={db}
+    imports={imports}
+    hashRouter={false}
+    websocketUrl="ws://127.0.0.1:60505"
+  />
+)
 
 export default hot(module)(Root)
--- doczrc.js
+++ doczrc.js
@@ -1,7 +1,4 @@
-import { babel } from 'docz-plugin-babel6';
-
 export default {
     title: 'React project',
-    description: 'Fronend documentation',
-    plugins: [babel()]
+    description: 'Fronend documentation'
 };
