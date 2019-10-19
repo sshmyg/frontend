4ab72ff255c015ac2253a2ee21be3b671eece0e9
Serhey Shmyg <serhey.shmyg@gmail.com>
2019-10-19 14:36:53 +0300

    Replace docz with storybook

--- .docz/app/imports.js
+++
@@ -1,4 +0,0 @@
-export const imports = {
-  'src/components/Button/readme.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "src-components-button-readme" */ 'src/components/Button/readme.mdx'),
-}
