353bb777da925c71ee6d086ae6258f664d658ce3
Serhey Shmyg <serhey.shmyg@gmail.com>
2021-01-19 18:57:24 +0200

    fix: storybook, update packages

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,4 +2,17 @@ const custom = require('../webpack.config.js');
 
 module.exports = {
   stories: ['../src/**/*.stories.@(js|mdx)'],
+  webpackFinal: (config) => {
+    return {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        alias: custom.resolve.alias,
+      },
+      module: { ...config.module, rules: custom.module.rules },
+    };
+  },
+  reactOptions: {
+    fastRefresh: true,
+  },
 };
