7f253ebe69b57d7ad0b046a149190889a6f4c6f4
Serhey Shmyg <serhey.shmyg@gmail.com>
2021-12-22 15:09:36 +0200

    fix: hotreload improvements 2

--- babel.config.js
+++ babel.config.js
@@ -1,38 +1,41 @@
-module.exports = {
-  presets: [
-    [
+module.exports = (api) => {
+  api.cache.using(() => process.env.NODE_ENV);
+
+  return {
+    presets: [
       '@babel/preset-env',
-      {
-        useBuiltIns: 'entry',
-        modules: false,
-        corejs: 3,
-      },
+
+      '@babel/preset-typescript',
+      [
+        '@babel/preset-react',
+        { development: !api.env('production'), runtime: 'automatic' },
+      ],
     ],
 
-    '@babel/preset-typescript',
-    '@babel/preset-react',
-  ],
+    plugins: [
+      '@babel/plugin-transform-async-to-generator',
+      '@babel/plugin-transform-runtime',
+    ],
 
-  plugins: [
-    '@babel/plugin-transform-async-to-generator',
-    '@babel/plugin-transform-runtime',
-  ],
+    env: {
+      development: {
+        plugins: [
+          'react-refresh/babel',
+          '@babel/plugin-transform-react-jsx-source',
+        ],
+      },
 
-  env: {
-    development: {
-      plugins: ['@babel/plugin-transform-react-jsx-source'],
+      production: {
+        plugins: [
+          'babel-plugin-transform-react-remove-prop-types',
+          '@babel/plugin-transform-react-constant-elements',
+          '@babel/plugin-transform-react-inline-elements',
+        ],
+      },
+
+      test: {
+        plugins: ['@babel/plugin-transform-modules-commonjs'],
+      },
     },
-
-    production: {
-      plugins: [
-        'babel-plugin-transform-react-remove-prop-types',
-        '@babel/plugin-transform-react-constant-elements',
-        '@babel/plugin-transform-react-inline-elements',
-      ],
-    },
-
-    test: {
-      plugins: ['@babel/plugin-transform-modules-commonjs'],
-    },
-  },
+  };
 };
--- webpack.config.js
+++ webpack.config.js
@@ -107,14 +107,7 @@ module.exports = {
       {
         test: /\.[jt]sx?$/,
         exclude: /node_modules/,
-        use: {
-          loader: require.resolve('babel-loader'),
-          options: {
-            plugins: [isDev && require.resolve('react-refresh/babel')].filter(
-              Boolean,
-            ),
-          },
-        },
+        use: 'babel-loader',
       },
 
       {
